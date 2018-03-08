d993aabf25d8ed7cdbc2d22a8640f2a16a1faedd
abra2abra <abra2abra@mailinator.com>
2018-03-08 23:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holidayName, nrDays){
+  return `It's ${nrDays} days until ${holidayName}!`; 
+}
+
